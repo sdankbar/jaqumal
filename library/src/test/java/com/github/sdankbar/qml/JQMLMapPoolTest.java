efba00755712b649868983192504e2396ccd52a0
sdankbar <sdankbar@localhost>
2020-01-18 14:09:55 -0600

    Added unit test for JQMLMapPool.

---
+++ library/src/test/java/com/github/sdankbar/qml/JQMLMapPoolTest.java
@@ -0,0 +1,84 @@
+/**
+ * The MIT License
+ * Copyright © 2019 Stephen Dankbar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.sdankbar.qml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.github.sdankbar.qml.eventing.NullEventFactory;
+import com.github.sdankbar.qml.models.JQMLMapPool;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Tests JQMLMapPool.
+ */
+public class JQMLMapPoolTest {
+
+	/**
+	 *
+	 */
+	public static interface EventProcessor {
+		// Empty Implementation
+	}
+
+	private enum Roles {
+		R1, R2, R3, R4, R5;
+	}
+
+	/**
+	 *
+	 */
+	@After
+	public void cleanup() {
+		JQMLApplication.delete();
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void requestRelease() {
+		final String[] args = new String[0];
+		final JQMLApplication<EventProcessor> app = JQMLApplication.create(args, new NullEventFactory<>());
+		final JQMLMapPool<Roles> pool = app.getModelFactory().createPool("other", Roles.class,
+				ImmutableMap.of(Roles.R1, new JVariant("INIT")));
+
+		final Map<Roles, JVariant> map = pool.request();
+		assertEquals("INIT", map.get(Roles.R1).asString());
+
+		map.put(Roles.R1, new JVariant("2"));
+		assertEquals("2", map.get(Roles.R1).asString());
+
+		pool.release(map);
+		assertEquals("INIT", map.get(Roles.R1).asString());
+
+		final Map<Roles, JVariant> map2 = pool.request();
+		assertSame(map, map2);
+	}
+
+}
