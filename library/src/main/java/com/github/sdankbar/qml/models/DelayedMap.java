c543adabe55a6911c38f95c45114d8fcf42ceacd
sdankbar <sdankbar@localhost>
2020-02-29 10:37:31 -0600

    Added the DelayedMap utility class.
    - DelayedMaps allows writes to another Map to be batched and flushed at
    a later time.

---
+++ library/src/main/java/com/github/sdankbar/qml/models/DelayedMap.java
@@ -0,0 +1,236 @@
+/**
+ * The MIT License
+ * Copyright © 2019 Stephen Dankbar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.sdankbar.qml.models;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * A DelayedMap wraps another map and allows for writes to the DelayedMap to be
+ * forwarded to the wrapped map in batches. If writes are delayed, getters do
+ * not return the delayed values. Any writes performed on the collections
+ * returned by keySet(), entrySet(), or values() bypass this classes delay logic
+ * so are immediate and thus should be avoided. Direct writes to the wrapped Map
+ * should also be avoided due to operations like clear() not working properly in
+ * that case.
+ *
+ * When in DELAYED WriteMode, clear, put, and remove operations respect the
+ * order that they are called. So if remove("A") is called then put("A", value)
+ * is called, after the Map is flushed, the wrapped Map will contain "A"/value.
+ * Same with clear() so any put() calls after the clear() will be written to the
+ * wrapped Map.
+ *
+ * Class is not thread safe.
+ *
+ * @param <K> The type for the Map's key
+ * @param <V> The type for the Map's value
+ */
+public class DelayedMap<K, V> implements Map<K, V> {
+
+	/**
+	 * Enumeration of the various write modes for DelayedMap
+	 */
+	public static enum WriteMode {
+		/**
+		 * Writes are immediately sent to the wrapped Map
+		 */
+		IMMEDIATE,
+		/**
+		 * Writes are held until the DelayedMap is flushed
+		 */
+		DELAYED
+	}
+
+	private final Map<K, V> wrapped;
+
+	private final Map<K, V> newValues = new HashMap<>();
+	private final Set<Object> removedKeys = new HashSet<>();
+
+	private boolean immediateWrites = true;
+
+	/**
+	 * Constructs a new DelayedMap that wraps the provided Map. Defaults to
+	 * WriteMode.IMMEDIATE.
+	 *
+	 * @param wrapped The Map to wrap.
+	 * @throws NullPointerException Thrown if wrapped is null.
+	 */
+	public DelayedMap(final Map<K, V> wrapped) {
+		this.wrapped = Objects.requireNonNull(wrapped, "wrapped is null");
+	}
+
+	/**
+	 * Constructs a new DelayedMap that wraps the provided Map. Defaults to
+	 * WriteMode.IMMEDIATE.
+	 *
+	 * @param wrapped          The Map to wrap.
+	 * @param initialWriteMode The DelayedMap's initial WriteMode
+	 * @throws NullPointerException Thrown if wrapped or initialWriteMode is null.
+	 */
+	public DelayedMap(final Map<K, V> wrapped, final WriteMode initialWriteMode) {
+		this.wrapped = Objects.requireNonNull(wrapped, "wrapped is null");
+		immediateWrites = WriteMode.IMMEDIATE == Objects.requireNonNull(initialWriteMode, "initialWriteMode is null");
+	}
+
+	@Override
+	public void clear() {
+		if (immediateWrites) {
+			wrapped.clear();
+		} else {
+			newValues.clear();
+			removedKeys.addAll(wrapped.keySet());
+		}
+	}
+
+	@Override
+	public boolean containsKey(final Object key) {
+		return wrapped.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(final Object value) {
+		return wrapped.containsValue(value);
+	}
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		return wrapped.entrySet();
+	}
+
+	/**
+	 * @return True if obj equals the wrapped Map.
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		return wrapped.equals(obj);
+	}
+
+	/**
+	 * Flushes any delayed writes to the wrapped Map.
+	 */
+	public void flush() {
+		for (final Object key : removedKeys) {
+			wrapped.remove(key);
+		}
+		removedKeys.clear();
+
+		wrapped.putAll(newValues);
+		newValues.clear();
+	}
+
+	@Override
+	public V get(final Object key) {
+		return wrapped.get(key);
+	}
+
+	/**
+	 * @return The wrapped Map's hashCode.
+	 */
+	@Override
+	public int hashCode() {
+		return wrapped.hashCode();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return wrapped.isEmpty();
+	}
+
+	@Override
+	public Set<K> keySet() {
+		return wrapped.keySet();
+	}
+
+	/**
+	 * @return The current value in the wrapped Map, regardless of WriteMode. So if
+	 *         there was a previous call to put() with the same key that has not
+	 *         been flushed, that value will not be returned.
+	 */
+	@Override
+	public V put(final K key, final V value) {
+		if (immediateWrites) {
+			return wrapped.put(key, value);
+		} else {
+			newValues.put(key, value);
+			removedKeys.remove(key);
+			return wrapped.get(key);
+		}
+	}
+
+	@Override
+	public void putAll(final Map<? extends K, ? extends V> values) {
+		if (immediateWrites) {
+			wrapped.putAll(values);
+		} else {
+			newValues.putAll(values);
+			removedKeys.removeAll(values.keySet());
+		}
+	}
+
+	@Override
+	public V remove(final Object key) {
+		if (immediateWrites) {
+			return wrapped.remove(key);
+		} else {
+			newValues.remove(key);
+			removedKeys.add(key);
+			return wrapped.get(key);
+		}
+	}
+
+	/**
+	 * Sets the write mode for the DelayMap to use. If going from DELAYED to
+	 * IMMEDIATE, automatically flushes the DELAYED data.
+	 *
+	 * @param mode The new write mode.
+	 * @throws NullPointerException Thrown if mode is null.
+	 */
+	public void setWriteMode(final WriteMode mode) {
+		final boolean oldImmediateWrites = immediateWrites;
+		immediateWrites = WriteMode.IMMEDIATE == Objects.requireNonNull(mode, "mode is null");
+		if (!oldImmediateWrites && immediateWrites) {
+			flush();
+		}
+	}
+
+	@Override
+	public int size() {
+		return wrapped.size();
+	}
+
+	@Override
+	public String toString() {
+		return "DelayedMap [wrapped=" + wrapped + ", newValues=" + newValues + ", removedKeys=" + removedKeys
+				+ ", immediateWrites=" + immediateWrites + "]";
+	}
+
+	@Override
+	public Collection<V> values() {
+		return wrapped.values();
+	}
+
+}
---
+++ library/src/test/java/com/github/sdankbar/qml/DelayedMapTest.java
@@ -0,0 +1,170 @@
+/**
+ * The MIT License
+ * Copyright © 2019 Stephen Dankbar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.sdankbar.qml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.github.sdankbar.qml.models.DelayedMap;
+import com.github.sdankbar.qml.models.DelayedMap.WriteMode;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Tests the DelayedMap class.
+ */
+public class DelayedMapTest {
+
+	/**
+	 *
+	 */
+	@Test
+	public void test_delayedWrites() {
+		final Map<String, String> wrapped = new HashMap<>();
+		final DelayedMap<String, String> delayed = new DelayedMap<>(wrapped, WriteMode.DELAYED);
+
+		assertEquals(null, delayed.put("A", "1"));
+		assertEquals(null, delayed.get("A"));
+		assertEquals(null, delayed.put("A", "2"));
+
+		delayed.putAll(ImmutableMap.of("B", "3", "C", "4"));
+		assertEquals(null, delayed.get("B"));
+		assertEquals(0, delayed.size());
+
+		assertEquals(null, delayed.remove("C"));
+		assertEquals(0, delayed.size());
+
+		delayed.flush();
+		assertEquals("2", delayed.get("A"));
+		assertEquals("3", delayed.get("B"));
+		assertEquals(2, delayed.size());
+
+		delayed.clear();
+		assertEquals(2, delayed.size());
+
+		assertEquals(null, delayed.put("Z", "26"));
+		delayed.flush();
+
+		assertEquals(1, delayed.size());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void test_delayedWrites_thenSwitchToImmediate() {
+		final Map<String, String> wrapped = new HashMap<>();
+		final DelayedMap<String, String> delayed = new DelayedMap<>(wrapped, WriteMode.DELAYED);
+
+		assertEquals(null, delayed.put("A", "1"));
+		assertEquals(null, delayed.get("A"));
+		assertEquals(null, delayed.put("A", "2"));
+
+		delayed.putAll(ImmutableMap.of("B", "3", "C", "4"));
+		assertEquals(null, delayed.get("B"));
+		assertEquals(0, delayed.size());
+
+		assertEquals(null, delayed.remove("C"));
+		assertEquals(0, delayed.size());
+
+		delayed.setWriteMode(WriteMode.IMMEDIATE);
+		assertEquals("2", delayed.get("A"));
+		assertEquals("3", delayed.get("B"));
+		assertEquals(2, delayed.size());
+	}
+
+	/**
+	 *
+	 */
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void test_getters() {
+		final Map<String, String> wrapped = new HashMap<>();
+		wrapped.put("A", "1");
+		wrapped.put("B", "2");
+		wrapped.put("C", "3");
+
+		final DelayedMap<String, String> delayed = new DelayedMap<>(wrapped, WriteMode.IMMEDIATE);
+
+		assertTrue(delayed.containsKey("A"));
+		assertTrue(delayed.containsKey("B"));
+		assertTrue(delayed.containsKey("C"));
+		assertFalse(delayed.containsKey("D"));
+
+		assertTrue(delayed.containsValue("1"));
+		assertFalse(delayed.containsValue("4"));
+
+		assertEquals(3, delayed.entrySet().size());
+
+		assertEquals("1", delayed.get("A"));
+		assertEquals(null, delayed.get("D"));
+
+		assertFalse(delayed.isEmpty());
+
+		assertEquals(3, delayed.keySet().size());
+
+		assertEquals(3, delayed.size());
+
+		assertEquals(3, delayed.values().size());
+
+		assertNotEquals(null, delayed.toString());
+
+		assertTrue(delayed.equals(wrapped));
+		assertTrue(delayed.equals(ImmutableMap.of("A", "1", "B", "2", "C", "3")));
+		assertTrue(!delayed.equals(ImmutableMap.of("A", "10", "B", "2", "C", "3")));
+
+		assertEquals(wrapped.hashCode(), delayed.hashCode());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void test_immediateWrites() {
+		final Map<String, String> wrapped = new HashMap<>();
+		final DelayedMap<String, String> delayed = new DelayedMap<>(wrapped);
+
+		assertEquals(null, delayed.put("A", "1"));
+		assertEquals("1", delayed.get("A"));
+		assertEquals("1", delayed.put("A", "2"));
+
+		delayed.putAll(ImmutableMap.of("B", "3", "C", "4"));
+		assertEquals("3", delayed.get("B"));
+		assertEquals(3, delayed.size());
+
+		assertEquals("4", delayed.remove("C"));
+		assertEquals(2, delayed.size());
+
+		delayed.clear();
+		assertTrue(delayed.isEmpty());
+
+		delayed.setWriteMode(WriteMode.DELAYED);
+	}
+
+}
