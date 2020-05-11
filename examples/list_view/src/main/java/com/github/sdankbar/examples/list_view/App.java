a931d49af9697d988c5405fb169954f37a8469ad
sdankbar <sdankbar@localhost>
2020-05-10 19:17:08 -0500

    Started a list_view example to demonstrate the JListView QML type.

---
+++ examples/list_view/src/main/java/com/github/sdankbar/examples/list_view/App.java
@@ -0,0 +1,66 @@
+/**
+ * The MIT License
+ * Copyright © 2019 Stephen Dankbar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.sdankbar.examples.list_view;
+
+import com.github.sdankbar.qml.JQMLApplication;
+import com.github.sdankbar.qml.JVariant;
+import com.github.sdankbar.qml.eventing.NullEventFactory;
+import com.github.sdankbar.qml.eventing.NullEventProcessor;
+import com.github.sdankbar.qml.models.list.JQMLListViewModel;
+import com.github.sdankbar.qml.models.list.JQMLListViewModel.SelectionMode;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Example application that allow for editing a set of colors.
+ *
+ */
+public class App {
+
+	public enum ListRole {
+		is_selected, text
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(final String[] args) throws Exception {
+		final JQMLApplication<NullEventProcessor> app = JQMLApplication.create(args, new NullEventFactory<>());
+		final JQMLListViewModel<ListRole> model = JQMLListViewModel.create("list_model", ListRole.class, app,
+				SelectionMode.SINGLE);
+
+		model.getModel().add(ImmutableMap.of(ListRole.text, new JVariant("Item 1")));
+		model.getModel().add(ImmutableMap.of(ListRole.text, new JVariant("Item 2")));
+		model.getModel().add(ImmutableMap.of(ListRole.text, new JVariant("Item 3")));
+		model.getModel().add(ImmutableMap.of(ListRole.text, new JVariant("Item 4")));
+		model.getModel().add(ImmutableMap.of(ListRole.text, new JVariant("Item 5")));
+
+		model.registerSelectionListener((changedIndices, selectedIndices, selected) -> {
+			System.out.println("Selection changed to:" + selectedIndices);
+		});
+
+		app.loadAndWatchQMLFile("./src/main/qml/main.qml");
+
+		app.execute();
+	}
+}
---
+++ examples/list_view/src/main/qml/main.qml
@@ -0,0 +1,62 @@
+﻿/**
+ * The MIT License
+ * Copyright © 2019 Stephen Dankbar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import QtQuick 2.10
+import QtQuick.Window 2.10
+import QtQuick.Controls 1.4
+import QtQuick.Dialogs 1.3
+import QtQuick.Controls.Styles 1.4
+import QtGraphicalEffects 1.0
+import com.github.sdankbar.jaqumal 0.4
+
+Window {
+    visible: true
+    width: 800
+    height: 600
+    x: 400
+    y: 400
+    title: qsTr("ListView")
+    id: mainWindow
+
+    EventBuilder {
+        id: eventing
+    }
+
+    ScrollView {
+		anchors.fill: parent
+		ListView {
+			anchors.fill: parent
+			spacing: 1
+            model: list_model
+            delegate: Rectangle {
+                width: 50
+                height: 20
+                color: model.is_selected ? "light blue" : "transparent"
+                Text {
+                    text: model.text
+                }
+            }
+        }
+	}
+
+
+}
