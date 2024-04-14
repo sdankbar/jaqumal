b5fc62a785789a4d8dad1e37bec8dac0f368e128
Stephen Dankbar <43022481+sdankbar@users.noreply.github.com>
2024-04-14 14:46:30 -0500

    Update table in example.

--- examples/lazy_table/src/main/java/com/github/sdankbar/examples/lazy_table/App.java
+++ examples/lazy_table/src/main/java/com/github/sdankbar/examples/lazy_table/App.java
@@ -22,8 +22,11 @@
  */
 package com.github.sdankbar.examples.lazy_table;
 
+import java.time.Instant;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.github.sdankbar.qml.JQMLApplication;
 import com.github.sdankbar.qml.JVariant;
@@ -70,22 +73,45 @@ public class App {
 
 		model.setExclusionFunction(m -> (m.get(ListRole.temp).asInteger() % 3 == 0));
 
-		final long s = System.currentTimeMillis();
-		final Map<String, ImmutableMap<ListRole, JVariant>> dataMap = new LinkedHashMap<>();
-		for (int i = 0; i < 20000; ++i) {
-			final ImmutableMap.Builder<ListRole, JVariant> b = ImmutableMap.builder();
-			b.put(ListRole.temp, new JVariant(i));
-			b.put(ListRole.text, new JVariant("Test " + Integer.toString(i)));
-			b.put(ListRole.text1, new JVariant("Test " + Integer.toString(20000 - i)));
-			b.put(ListRole.text2, new JVariant("Test " + Integer.toString(3 * i)));
-			b.put(ListRole.text3, new JVariant("Test " + Integer.toString(4 * i)));
-			b.put(ListRole.text4, new JVariant("Test " + Integer.toString(5 * i)));
-			b.put(ListRole.text5, new JVariant("Test " + Integer.toString(6 * i)));
-			b.put(ListRole.text6, new JVariant("Test " + Integer.toString(7 * i)));
-			dataMap.put(Integer.toString(i), b.build());
+		{
+			final long s = System.currentTimeMillis();
+			final Map<String, ImmutableMap<ListRole, JVariant>> dataMap = new LinkedHashMap<>();
+			for (int i = 0; i < 20000; ++i) {
+				final ImmutableMap.Builder<ListRole, JVariant> b = ImmutableMap.builder();
+				b.put(ListRole.temp, new JVariant(i));
+				b.put(ListRole.text, new JVariant("Test " + Integer.toString(i)));
+				b.put(ListRole.text1, new JVariant("Test " + Integer.toString(20000 - i)));
+				b.put(ListRole.text2, new JVariant("Test " + Integer.toString(3 * i)));
+				b.put(ListRole.text3, new JVariant("Test " + Integer.toString(4 * i)));
+				b.put(ListRole.text4, new JVariant("Test " + Integer.toString(5 * i)));
+				b.put(ListRole.text5, new JVariant("Test " + Integer.toString(6 * i)));
+				b.put(ListRole.text6, new JVariant("Test " + Integer.toString(7 * i)));
+				dataMap.put(Integer.toString(i), b.build());
+			}
+			model.upsertAll(dataMap);
+			System.out.println("Took " + (System.currentTimeMillis() - s) + " milliseconds");
 		}
-		model.upsertAll(dataMap);
-		System.out.println("Took " + (System.currentTimeMillis() - s) + " milliseconds");
+
+		final AtomicInteger offsetRef = new AtomicInteger();
+		app.getQMLThreadExecutor().scheduleAtFixedRate(() -> {
+			final long s = System.currentTimeMillis();
+			final Map<String, ImmutableMap<ListRole, JVariant>> dataMap = new LinkedHashMap<>();
+			final int offset = offsetRef.getAndIncrement();
+			for (int i = 0; i < 20000; ++i) {
+				final ImmutableMap.Builder<ListRole, JVariant> b = ImmutableMap.builder();
+				b.put(ListRole.temp, new JVariant(i));
+				b.put(ListRole.text, new JVariant("Test " + Integer.toString(i + offset)));
+				b.put(ListRole.text1, new JVariant("Test " + Integer.toString(20000 - i + offset)));
+				b.put(ListRole.text2, new JVariant("Test " + Integer.toString(3 * i + offset)));
+				b.put(ListRole.text3, new JVariant("Test " + Integer.toString(4 * i + offset)));
+				b.put(ListRole.text4, new JVariant("Test " + Integer.toString(5 * i + offset)));
+				b.put(ListRole.text5, new JVariant("Test " + Integer.toString(6 * i + offset)));
+				b.put(ListRole.text6, new JVariant("Test " + Integer.toString(7 * i + offset)));
+				dataMap.put(Integer.toString(i), b.build());
+			}
+			model.upsertAll(dataMap);
+			System.out.println(Instant.now() + " - Took " + (System.currentTimeMillis() - s) + " milliseconds");
+		}, 2000, 100, TimeUnit.MILLISECONDS);
 
 		app.loadAndWatchQMLFile("./src/main/qml/main.qml");
 
